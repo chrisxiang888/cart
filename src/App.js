c3a2280b64cd20bac5d1ed3e2c79b09f940ef5d7
chrisxiang888 <chrisxiang86@hotmail.com>
2022-05-14 16:17:39 -0400

    deploy my page

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import './App.css';
+import Header from './components/Header';
+import {BrowserRouter,Routes,
+  Route} from "react-router-dom"
+  import Home from './components/Home';
+ import CartPage from './components/Cart';
+function App() {
+  
+ 
+  return (
+    <BrowserRouter>
+     <Header/>
+     <div className='App'>
+       <Routes>
+       <Route path='/' element={<Home />}/>
+       <Route path='/cart' element={<CartPage/>}/>
+       </Routes>
+       
+     </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
